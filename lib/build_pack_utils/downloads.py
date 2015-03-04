6b087ebbf65c50cd45c1f356547ac17acae18d15
Michel Del Piero <michel.delpiero1@swisscom.com>
2015-03-04 17:55:00 +0100

    mods for s3 secure cache downloads

--- lib/build_pack_utils/downloads.py
+++ lib/build_pack_utils/downloads.py
@@ -35,6 +35,7 @@ class Downloader(object):
         self._log.info('Downloaded [%s] to [%s]', url, toFile)
 
     def download_direct(self, url):
+        print("URL: %s" % url)
         buf = urllib2.urlopen(url).read()
         self._log.info('Downloaded [%s] to memory', url)
         self._log.debug("Downloaded [%s] [%s]", url, buf)
